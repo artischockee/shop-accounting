defe55d2162f4064ea31777f507f0d58c35565dc
artyeug <artyeug@gmail.com>
2018-10-25 00:56:01 +0700

    Add 2 new sections

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Header from '../containers/Header';
+import Main from '../containers/Main';
 
 const App = (props) => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        {'< shop-accounting />'}
-      </p>
-    </header>
+  <div className="app">
+    <Header />
+    <Main />
   </div>
 );
 
