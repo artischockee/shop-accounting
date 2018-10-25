a9c00c24a57e53b808216d0b1fca690d7be9ca27
artyeug <artyeug@gmail.com>
2018-10-25 18:08:17 +0700

    Inline <AddProdDialog /> rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../containers/Header';
 import Main from '../containers/Main';
-
 import AddProdDialog from '../containers/AddProdDialog';
-
-const getAddProdDialog = (shouldBeDisplayed) => (
-  shouldBeDisplayed
-    ? <AddProdDialog />
-    : null
-)
 
 const App = ({ showAddProdDialog }) => (
   <div className="app">
-    {getAddProdDialog(showAddProdDialog)}
+
+    {showAddProdDialog ? <AddProdDialog /> : null}
+
     <Header />
+    
     <Main />
   </div>
 );
