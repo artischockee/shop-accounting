ccbab5bb5f1bcd672cebb219aae2cb11e6501e34
artyeug <artyeug@gmail.com>
2018-10-25 18:11:05 +0700

    Add dispatching an action; add stateToProps fields

--- src/containers/ProductCard.js
+++ src/containers/ProductCard.js
@@ -1,12 +1,20 @@
 import { connect } from 'react-redux';
+import { switchSettingsMenu } from '../actions';
 import ProductCard from '../components/ProductCard';
 
-const mapStateToProps = (state, ownProps) => ({
+const getResult = (products, ownId) => (
+  products.find((prod) => prod.id === ownId).displaySettingsMenu
+    ? true
+    : false
+);
 
+const mapStateToProps = (state, ownProps) => ({
+  displaySettingsMenu: getResult(state.products, ownProps.id),
+  id: ownProps.id
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-
+  switchSettingsMenu: () => dispatch(switchSettingsMenu(ownProps.id))
 });
 
 export default connect(
