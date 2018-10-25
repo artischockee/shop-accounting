5ca054547ae84a532c08fccebcb79024715b70b8
artyeug <artyeug@gmail.com>
2018-10-25 18:07:57 +0700

    Add new actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const Products = {
-  ADD_PRODUCT: 'ADD_PRODUCT'
+  ADD_PRODUCT: 'ADD_PRODUCT',
+  DELETE_PRODUCT: 'DELETE_PRODUCT'
 };
 
 let productId = 8; // value is 8 because of existing products (see 'reducers/products')
@@ -9,6 +10,12 @@ export const addProduct = (name, quantity) => ({
   name,
   quantity
 });
+
+export const deleteProduct = (id) => ({
+  type: Products.DELETE_PRODUCT,
+  id
+});
+
 
 export const AddProdDialog = {
   OPEN: 'OPEN',
@@ -23,9 +30,18 @@ export const closeAddProdDialog = () => ({
   type: AddProdDialog.CLOSE
 });
 
+
 export const SEARCH_VALUE_CHANGE = 'SEARCH_VALUE_CHANGE';
 
 export const searchValueChange = (value) => ({
   type: SEARCH_VALUE_CHANGE,
   value
+});
+
+
+export const SWITCH_PRODUCT_SETTINGS_MENU = 'SWITCH_PRODUCT_SETTINGS_MENU';
+
+export const switchSettingsMenu = (productId) => ({
+  type: SWITCH_PRODUCT_SETTINGS_MENU,
+  id: productId
 });
