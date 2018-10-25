e10c863326254f980a9fa4dc81f82ab2dad2eff5
artyeug <artyeug@gmail.com>
2018-10-25 12:52:17 +0700

    Add showAddProdDialog state

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import App from '../components/App';
 
 const mapStateToProps = (state, ownProps) => ({
-
+  showAddProdDialog: state.showAddProdDialog
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
