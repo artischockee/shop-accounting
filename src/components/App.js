36ede9214aa1558f0abaaa23a47810e21858d4f5
artyeug <artyeug@gmail.com>
2018-10-25 12:52:53 +0700

    Add getAddProdDialog function

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../containers/Header';
 import Main from '../containers/Main';
 
-const App = (props) => (
+import AddProdDialog from '../containers/AddProdDialog';
+
+const getAddProdDialog = (shouldBeDisplayed) => (
+  shouldBeDisplayed
+    ? <AddProdDialog />
+    : null
+)
+
+const App = ({ showAddProdDialog }) => (
   <div className="app">
+    {getAddProdDialog(showAddProdDialog)}
     <Header />
     <Main />
   </div>
 );
+
+App.propTypes = {
+  showAddProdDialog: PropTypes.bool.isRequired
+};
 
 export default App;
