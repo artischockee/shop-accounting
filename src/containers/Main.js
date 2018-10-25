2c0647ae0884d3665d84b365ff952e2f8230b08b
artyeug <artyeug@gmail.com>
2018-10-25 12:26:28 +0700

    Add products filtering (by search input) function

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,8 +1,21 @@
 import { connect } from 'react-redux';
 import Main from '../components/Main';
 
+const getProductsBySearchValue = (products, searchValue) => {
+  if (searchValue === '')
+    return products;
+
+  searchValue = searchValue.toLowerCase();
+
+  return products.filter((product) => {
+    let name = product.name.toLowerCase();
+
+    return name.includes(searchValue);
+  });
+};
+
 const mapStateToProps = (state, ownProps) => ({
-  products: state.products
+  products: getProductsBySearchValue(state.products, state.searchValue)
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
