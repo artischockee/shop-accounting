922a5cacde45044b8a7df927d5108f93176e6047
artyeug <artyeug@gmail.com>
2018-10-25 12:27:41 +0700

    Add new searchValueChange action

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,10 @@ export const addProduct = (product) => ({
   type: Products.ADD_PRODUCT,
   product
 });
+
+export const SEARCH_VALUE_CHANGE = 'SEARCH_VALUE_CHANGE';
+
+export const searchValueChange = (value) => ({
+  type: SEARCH_VALUE_CHANGE,
+  value
+});
