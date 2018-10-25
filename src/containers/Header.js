79761df9ca3cd6a7fcce4469a3a7b8cc9ec9b7d2
artyeug <artyeug@gmail.com>
2018-10-25 12:27:25 +0700

    Implement search module

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
+import { searchValueChange } from '../actions';
 import Header from '../components/Header';
 
 const mapStateToProps = (state, ownProps) => ({
-
+  searchValue: state.searchValue
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-
+  onInputChange: (e) => dispatch(searchValueChange(e.target.value))
 });
 
 export default connect(
