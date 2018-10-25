9334245f1326edaf0a84b2c355a5d2491cf7fab8
artyeug <artyeug@gmail.com>
2018-10-25 12:51:43 +0700

    Add showAddProdDialog reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import products from './products';
 import searchValue from './searchValue';
+import showAddProdDialog from './showAddProdDialog';
 
 export default combineReducers({
   products,
-  searchValue
+  searchValue,
+  showAddProdDialog
 });
