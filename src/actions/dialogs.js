dc99b154fd0a4156542f93115e0c7dcc32d3dc00
artyeug <artyeug@gmail.com>
2018-10-26 15:02:14 +0700

    Modify Dialogs fields for different dialog types

--- src/actions/dialogs.js
+++ src/actions/dialogs.js
@@ -1,24 +1,34 @@
 export const Dialogs = {
-  OPEN: 'OPEN',
-  CLOSE: 'CLOSE'
+  OPEN_ADD: 'OPEN_ADD',
+  OPEN_EDIT: 'OPEN_EDIT',
+  CLOSE_ADD: 'CLOSE_ADD',
+  CLOSE_EDIT: 'CLOSE_EDIT',
+  CLOSE_ALL: 'CLOSE_ALL'
 };
+
+// Close all dialogs
+
+export const closeAllDialogs = () => ({
+  type: Dialogs.CLOSE_ALL
+});
 
 // Add product
 
 export const openAddProdDialog = () => ({
-  type: Dialogs.OPEN
+  type: Dialogs.OPEN_ADD
 });
 
 export const closeAddProdDialog = () => ({
-  type: Dialogs.CLOSE
+  type: Dialogs.CLOSE_ADD
 });
 
 // Edit product
 
-export const openEditProdDialog = () => ({
-  type: Dialogs.OPEN
+export const openEditProdDialog = (id) => ({
+  type: Dialogs.OPEN_EDIT,
+  id
 });
 
 export const closeEditProdDialog = () => ({
-  type: Dialogs.CLOSE
+  type: Dialogs.CLOSE_EDIT
 });
