63d63b41bbdc80f2d4af607d9d399c178c6ad5c5
artyeug <artyeug@gmail.com>
2018-10-26 15:01:44 +0700

    Update propTypes; add EditProdDialog component

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import Header from '../containers/Header';
 import Main from '../containers/Main';
 import AddProdDialog from '../containers/AddProdDialog';
+import EditProdDialog from '../containers/EditProdDialog';
 
-const App = ({ showAddProdDialog }) => (
+const App = ({
+  showAddProdDialog,
+  showEditProdDialog
+}) => (
   <div className="app">
 
     {showAddProdDialog ? <AddProdDialog /> : null}
+    {showEditProdDialog ? <EditProdDialog /> : null}
 
     <Header />
 
@@ -16,7 +21,8 @@ const App = ({ showAddProdDialog }) => (
 );
 
 App.propTypes = {
-  showAddProdDialog: PropTypes.bool.isRequired
+  showAddProdDialog: PropTypes.bool.isRequired,
+  showEditProdDialog: PropTypes.bool.isRequired
 };
 
 export default App;
