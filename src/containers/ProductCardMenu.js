b6456147b43ae4bc2520e68de737b3d1600d94db
artyeug <artyeug@gmail.com>
2018-10-26 13:08:17 +0700

    Make getClassName function more DRY

--- src/containers/ProductCardMenu.js
+++ src/containers/ProductCardMenu.js
@@ -2,18 +2,21 @@ import { connect } from 'react-redux';
 import { deleteProduct } from '../actions';
 import ProductCardMenu from '../components/ProductCardMenu';
 
-const getClassName = (isVisible) => (
-  isVisible
-    ? 'product-card__menu product-card__menu_visible'
-    : 'product-card__menu'
-);
+const getClassName = (isVisible) => {
+  let className = 'product-card__menu';
+
+  return isVisible
+    ? `${className} ${className}_visible`
+    : className
+};
 
 const mapStateToProps = (state, ownProps) => ({
   className: getClassName(ownProps.visible)
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  deleteProduct: () => dispatch(deleteProduct(ownProps.id))
+  deleteProduct: () => dispatch(deleteProduct(ownProps.id)),
+  showEditProdDialog: () => {}
 });
 
 export default connect(
