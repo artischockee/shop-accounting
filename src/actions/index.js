072734fac0ba06dd350364879aec28fdca10de00
artyeug <artyeug@gmail.com>
2018-10-26 13:04:24 +0700

    Extract some actions into different files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,35 +1,5 @@
-export const Products = {
-  ADD_PRODUCT: 'ADD_PRODUCT',
-  DELETE_PRODUCT: 'DELETE_PRODUCT'
-};
-
-let productId = 8; // value is 8 because of existing products (see 'reducers/products')
-export const addProduct = (name, quantity) => ({
-  type: Products.ADD_PRODUCT,
-  id: productId++,
-  name,
-  quantity
-});
-
-export const deleteProduct = (id) => ({
-  type: Products.DELETE_PRODUCT,
-  id
-});
-
-
-export const AddProdDialog = {
-  OPEN: 'OPEN',
-  CLOSE: 'CLOSE'
-};
-
-export const openAddProdDialog = () => ({
-  type: AddProdDialog.OPEN
-});
-
-export const closeAddProdDialog = () => ({
-  type: AddProdDialog.CLOSE
-});
-
+export * from './products';
+export * from './dialogs';
 
 export const SEARCH_VALUE_CHANGE = 'SEARCH_VALUE_CHANGE';
 
@@ -37,7 +7,6 @@ export const searchValueChange = (value) => ({
   type: SEARCH_VALUE_CHANGE,
   value
 });
-
 
 export const SWITCH_PRODUCT_SETTINGS_MENU = 'SWITCH_PRODUCT_SETTINGS_MENU';
 
