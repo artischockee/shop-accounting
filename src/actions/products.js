f5203e9f6ee5686b72504e02a283bdd95c5003c2
artyeug <artyeug@gmail.com>
2018-10-27 11:13:01 +0700

    Add editProduct action

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,6 +1,7 @@
 export const Products = {
   ADD_PRODUCT: 'ADD_PRODUCT',
-  DELETE_PRODUCT: 'DELETE_PRODUCT'
+  DELETE_PRODUCT: 'DELETE_PRODUCT',
+  EDIT_PRODUCT: 'EDIT_PRODUCT'
 };
 
 let productId = 8; // value is 8 because of existing products (see 'reducers/products')
@@ -14,4 +15,9 @@ export const addProduct = (name, quantity) => ({
 export const deleteProduct = (id) => ({
   type: Products.DELETE_PRODUCT,
   id
+});
+
+export const editProduct = (product) => ({
+  type: Products.EDIT_PRODUCT,
+  product
 });
