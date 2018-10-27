3c123c5ae484dc5efaff7cd7ba5ff47407bf9923
artyeug <artyeug@gmail.com>
2018-10-27 23:18:57 +0700

    Separate switchSettingsMenu into 2 dispatchers (onMouse(Enter|Leave))

--- src/containers/ProductCard.js
+++ src/containers/ProductCard.js
@@ -1,17 +1,14 @@
 import { connect } from 'react-redux';
-import { switchSettingsMenu } from '../actions';
+import { showProdSettingsMenu, hideProdSettingsMenu } from '../actions';
 import ProductCard from '../components/ProductCard';
 
-const getResult = (products, ownId) => (
-  products.find((prod) => prod.id === ownId).displaySettingsMenu
-);
-
 const mapStateToProps = (state, ownProps) => ({
-  isSettingsMenuVisible: getResult(state.products, ownProps.id)
+  isSettingsMenuVisible: state.displayMenuForId === ownProps.id
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  switchSettingsMenu: () => dispatch(switchSettingsMenu(ownProps.id))
+  hideProductCardMenu: () => dispatch(hideProdSettingsMenu()),
+  showProductCardMenu: () => dispatch(showProdSettingsMenu(ownProps.id))
 });
 
 export default connect(
