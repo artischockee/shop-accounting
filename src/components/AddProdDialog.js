bbc7aa1c6c7762fdc60488f3f0634dc5f8d6d0d4
artyeug <artyeug@gmail.com>
2018-10-27 21:11:19 +0700

    Connect the component and its fields with redux-form

--- src/components/AddProdDialog.js
+++ src/components/AddProdDialog.js
@@ -1,74 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addProduct } from '../actions';
+import { Field, reduxForm } from 'redux-form';
 import SVGClose from '../svg-components/Close';
 
 // TODO: Add a form validation and show warnings on wrong symbols typed
 
 const AddProdDialog = ({
-  dialogClose,
-  dispatch
-}) => {
-  let productName;
-  let productQuantity;
+  handleDialogClose,
+  handleSubmit
+}) => (
+  <div className="fullscreen-wrapper">
+    <form className="dialog" onSubmit={handleSubmit}>
+      <div className="dialog__title">
+        <p className="title__text">Add product</p>
+        <button type="button" className="title__button" onClick={handleDialogClose}>
+          <SVGClose className="button__svg" />
+        </button>
+      </div>
 
-  return (
-    <div className="fullscreen-wrapper">
-      <form className="dialog"
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          if (
-            !productName.value.trim() ||
-            isNaN(productQuantity.value)
-          ) {
-            return;
-          }
-
-          dispatch(addProduct(productName.value, Number(productQuantity.value)));
-
-          productName.value = '';
-          productQuantity.value = '';
-
-          dialogClose();
-        }}
-      >
-        <div className="dialog__title">
-          <p className="title__text">Add product</p>
-          <button type="button" className="title__button" onClick={dialogClose}>
-            <SVGClose className="button__svg" />
-          </button>
+      <div className="dialog__body">
+        <div className="body__field">
+          <label className="field__label" htmlFor="name">Product name</label>
+          <Field
+            className="field__input"
+            component="input"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Product name"
+          />
         </div>
-
-        <div className="dialog__body">
-          <div className="body__field">
-            <label className="field__label" htmlFor="name">Product name</label>
-            <input type="text" className="field__input" id="name"
-              placeholder="Product name"
-              ref={node => (productName = node)}
-            />
-          </div>
-          <div className="body__field">
-            <label className="field__label" htmlFor="quantity">Quantity</label>
-            <input type="text" className="field__input field__input_type_number" id="quantity"
-              placeholder="0"
-              ref={node => (productQuantity = node)}
-            />
-          </div>
+        <div className="body__field">
+          <label className="field__label" htmlFor="quantity">Quantity</label>
+          <Field
+            className="field__input field__input_type_number"
+            component="input"
+            type="text"
+            id="quantity"
+            name="quantity"
+            placeholder="0"
+          />
         </div>
+      </div>
 
-        <div className="dialog__controls">
-          <button type="button" className="controls__button" onClick={dialogClose}>Cancel</button>
-          <button type="submit" className="controls__button controls__button_primary">Add</button>
-        </div>
-      </form>
-    </div>
-  );
-};
+      <div className="dialog__controls">
+
+        <button
+          type="button"
+          className="controls__button"
+          onClick={handleDialogClose}
+        >
+          Cancel
+        </button>
+
+        <button
+          type="submit"
+          className="controls__button controls__button_primary"
+        >
+          Add
+        </button>
+
+      </div>
+
+    </form>
+  </div>
+);
 
 AddProdDialog.propTypes = {
-  dialogClose: PropTypes.func.isRequired,
-  dispatch: PropTypes.func.isRequired
+  handleDialogClose: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
 };
 
-export default AddProdDialog;
+export default reduxForm({
+  form: 'addProduct'
+})(AddProdDialog);
