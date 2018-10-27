4264e26f651c6d31f7e275ce40bddfa9c9b466eb
artyeug <artyeug@gmail.com>
2018-10-27 12:07:10 +0700

    Add appMenu reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,10 @@ import { reducer as formReducer } from 'redux-form';
 import products from './products';
 import searchValue from './searchValue';
 import dialogs from './dialogs';
+import appMenu from './appMenu';
 
 export default combineReducers({
+  appMenu,
   products,
   searchValue,
   dialogs,
