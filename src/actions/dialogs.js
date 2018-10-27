3d22e4b3fd1bfdb91eae08ecd081adf6b642f0ea
artyeug <artyeug@gmail.com>
2018-10-27 20:05:21 +0700

    Replace redundant 'close' dialog actions with the universal one

--- src/actions/dialogs.js
+++ src/actions/dialogs.js
@@ -1,34 +1,24 @@
 export const Dialogs = {
   OPEN_ADD: 'OPEN_ADD',
   OPEN_EDIT: 'OPEN_EDIT',
-  CLOSE_ADD: 'CLOSE_ADD',
-  CLOSE_EDIT: 'CLOSE_EDIT',
-  CLOSE_ALL: 'CLOSE_ALL'
+  OPEN_DELETE: 'OPEN_DELETE',
+  CLOSE_CURRENT: 'CLOSE_CURRENT'
 };
 
-// Close all dialogs
-
-export const closeAllDialogs = () => ({
-  type: Dialogs.CLOSE_ALL
+export const closeCurrentDialog = () => ({
+  type: Dialogs.CLOSE_CURRENT
 });
-
-// Add product
 
 export const openAddProdDialog = () => ({
   type: Dialogs.OPEN_ADD
 });
-
-export const closeAddProdDialog = () => ({
-  type: Dialogs.CLOSE_ADD
-});
-
-// Edit product
 
 export const openEditProdDialog = (id) => ({
   type: Dialogs.OPEN_EDIT,
   id
 });
 
-export const closeEditProdDialog = () => ({
-  type: Dialogs.CLOSE_EDIT
+export const openDeleteProdDialog = (id) => ({
+  type: Dialogs.OPEN_DELETE,
+  id
 });
